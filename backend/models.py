43ab2b8828d17b94082906345cd5a8ea4f720e0f
ivan <i_nevakshenov@mail.ru>
2023-04-11 22:36:32 +0500

    Добавлены остальные методы

--- backend/models.py
+++ backend/models.py
@@ -93,6 +93,8 @@ class User(AbstractUser):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
+
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = "Список пользователей"
